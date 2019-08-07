21121be06ec302cf58417c4ac0d4632a4ccca162
judy-njeru <njerujudy87@ymail.com>
2019-08-07 19:07:20 +0200

    added a carousel component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,30 @@
 <template>
     <div>
         <div id="nav">
-            <router-link to="/">Home</router-link> |
+            <b-container>
+                <b-row align-h="between">
+                    <b-col cols="2">
+                        <router-link to="/">Home</router-link> 
+                    </b-col>
+                    <b-col cols="2"> 
+                        <router-link to="/Restaurant">Restaurant</router-link> 
+                    </b-col>
+                    <b-col cols="2"> 
+                        <router-link to="/Products">Products</router-link> 
+                    </b-col>
+                    <b-col cols="2"> 
+                        <router-link to="/NewsLetter">NewsLetter</router-link> 
+                    </b-col>
+                    <b-col cols="2"> 
+                        <router-link to="/Contact">Contact</router-link> 
+                    </b-col>
+                </b-row>
+            </b-container>
+            <!-- <router-link to="/">Home</router-link> |
             <router-link to="/Restaurant">Restaurant</router-link> |
             <router-link to="/Products">Products</router-link> |
             <router-link to="/NewsLetter">NewsLetter</router-link> |
-            <router-link to="/Contact">Contact</router-link> 
+            <router-link to="/Contact">Contact</router-link>  -->
         </div>
         <!-- <router-view/> -->
     </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="8">
+          <Carousel />
+        </b-col>
+        <b-col sm="4">col-sm-4</b-col>
+      </b-row>
+    </b-container>
+    <!-- <HelloWorld /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Carousel from '@/components/Carousel.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Carousel
   }
 }
 </script>
